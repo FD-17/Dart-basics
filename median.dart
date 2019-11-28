6e2d786e6fd99f9f99d1b030c483d731a5a60a33
FD-17 <54020957+FD-17@users.noreply.github.com>
2019-11-28 14:54:12 +0530

    Update median.dart

--- median.dart
+++ median.dart
@@ -1,17 +1,17 @@
 import 'dart:io';
 void main() {
-var array=[1,2,3,21,9,6,4,7,78];
+var array=[1,2,3,21,6,4,7,78];
 var median=(array.length/2).floor();
 if(array.length%2!=0){
 var median=array.length~/2;
 stdout.write("Median is ${array[median]}");
 }
 else{
-median=(array[median-1]+array[median])~/2;
-stdout.write("Median is $median");
+double med=(array[median-1]+array[median])/2;
+stdout.write("Median is $med");
 }
 }
 
 /*
-Median is 9
+Median is 13.5
 */
